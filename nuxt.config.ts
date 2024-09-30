ff6525a6ebff8e5e133cd13c6eb35b311675f18e
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-09-30 15:16:31 +0200

    Added modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-								compatibilityDate: "2024-04-03",
-								devtools: { enabled: true },
-								modules: ["@nuxt/content", "@nuxt/image", "@nuxtjs/tailwindcss"],
-								css: ["~/assets/css/reset.css", "~/assets/css/main.css"],
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
+	modules: ["@nuxt/content", "@nuxt/image", "@nuxtjs/tailwindcss"],
+	css: ["~/assets/css/reset.css", "~/assets/css/main.css"],
 });
