570cd0bba32f5ec16509caa5ce217b6226b5da25
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-15 15:32:46 +0100

    Playwright init

---
+++ tests/home-fr.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("/fr");
+});
+
+test("has title", async ({ page }) => {
+	await expect(page).toHaveTitle("Portfolio personnel | Jérémie Zurflüh");
+});
+
+test.describe("navigation", () => {
+	test.describe("nav", () => {
+		test.beforeEach(async ({ page }) => {
+			await page.evaluate(() => {
+				document.querySelector("#personnal-presentation")?.scrollIntoView();
+			});
+		});
+
+		test("Nav Home link to stay", async ({ page }) => {
+			page.getByRole("link", { name: "Accueil" }).click();
+			await expect(page).toHaveURL(/\/fr$/);
+		});
+
+		test("Nav Portfolio link redirect", async ({ page }) => {
+			page.getByRole("link", { name: "Portfolio" }).click();
+			await expect(page).toHaveURL(/fr\/portfolio$/);
+		});
+
+		test("Nav About link redirect", async ({ page }) => {
+			page.getByRole("link", { name: "À propos" }).click();
+			await expect(page).toHaveURL(/\/fr\/about$/);
+		});
+	});
+
+	test.describe("social links", () => {
+		test("check Github link", async ({ page }) => {
+			const href = await page.locator(".awesome-social-button.github a").getAttribute("href");
+			expect(href).toBe("https://github.com/zurfjereluhmie");
+		});
+
+		test("check LinkedIn link", async ({ page }) => {
+			const href = await page.locator(".awesome-social-button.linkedin a").getAttribute("href");
+			expect(href).toBe("https://www.linkedin.com/in/jeremie-zurfluh/");
+		});
+	});
+
+	test("click the name to be redirected to /about", async ({ page }) => {
+		page.getByRole("link", { name: "Je suis Jérémie" }).click();
+		await expect(page).toHaveURL(/fr\/about$/);
+	});
+
+	test("click 'See my work' to be redirected to /portfolio", async ({ page }) => {
+		page.getByRole("link", { name: "Voir quelques-unes de mes réalisations" }).click();
+		await expect(page).toHaveURL(/fr\/portfolio$/);
+	});
+});
