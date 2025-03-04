cc9a305eb6329f1cf0fd1f91297d713b4c1a7701
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-03-04 13:36:54 +0100

    fix(favicons): icon.horse API is down at the moment, so replaced by Google one

--- components/content/AppMdLinks.vue
+++ components/content/AppMdLinks.vue
@@ -10,7 +10,7 @@ const cleanLinks = computed(() => {
 	return props.links.map((link) => new URL(link));
 });
 
-const BASE_FAVICON_API = "https://icon.horse/icon/";
+const BASE_FAVICON_API = "https://www.google.com/s2/favicons?domain=https://";
 </script>
 
 <template>
