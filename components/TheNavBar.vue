02e64e0ee9b1f8ff7a0437bb50c60f0bc0e8c971
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-09-16 11:36:52 +0200

    feat: working light/dark mode

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,38 +1,27 @@
 <script setup>
+import { Sun, Moon } from "lucide-vue-next";
+
 const { t } = useI18n();
 const localePath = useLocalePath();
 const props = defineProps(["currentPath"]);
-
-import { updateColor } from "~/assets/utils/colorVariation";
 
 const isActive = (path) => {
 	return props.currentPath === path ? "active" : "";
 };
 
-const timeOutId = ref(null);
+const colorMode = useColorMode();
 
-const updateClock = () => {
-	const now = new Date();
-	const seconds = now.getSeconds();
-	const minutes = now.getMinutes();
-	const hours = now.getHours();
-
-	const minuteAngle = (minutes / 60) * 360 + (seconds / 60) * 6;
-	const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30;
-
-	document.querySelector("#minute-hand").style.transform = `rotate(${minuteAngle}deg)`;
-	document.querySelector("#hour-hand").style.transform = `rotate(${hourAngle}deg)`;
-
-	updateColor(hours);
+const updateColor = (mode) => {
+	document.querySelector("html").dataset.theme = mode;
 };
 
 onMounted(() => {
-	updateClock();
-	timeOutId.value = setInterval(updateClock, 1000);
-});
-
-onUnmounted(() => {
-	clearInterval(timeOutId.value);
+	watchEffect(
+		() => {
+			updateColor(colorMode.value);
+		},
+		{ immediate: true }
+	);
 });
 </script>
 
@@ -57,13 +46,22 @@ onUnmounted(() => {
 		</ul>
 	</nav>
 	<div class="time-widget sm:hidden md:block">
-		<svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path
-				d="M464 256C464 311.165 442.086 364.071 403.078 403.078C364.071 442.086 311.165 464 256 464C200.835 464 147.929 442.086 108.922 403.078C69.9142 364.071 48 311.165 48 256C48 200.835 69.9142 147.929 108.922 108.922C147.929 69.9142 200.835 48 256 48C311.165 48 364.071 69.9142 403.078 108.922C442.086 147.929 464 200.835 464 256ZM0 256C0 323.895 26.9714 389.01 74.9807 437.019C122.99 485.029 188.105 512 256 512C323.895 512 389.01 485.029 437.019 437.019C485.029 389.01 512 323.895 512 256C512 188.105 485.029 122.99 437.019 74.9807C389.01 26.9714 323.895 0 256 0C188.105 0 122.99 26.9714 74.9807 74.9807C26.9714 122.99 0 188.105 0 256Z"
-			/>
-			<rect id="minute-hand" x="232" y="95" width="48" height="185" rx="24" />
-			<rect id="hour-hand" x="232" y="160" width="48" height="120" rx="24" />
-		</svg>
+		<Moon
+			v-if="colorMode.value === 'dark'"
+			@click="colorMode.value = 'light'"
+			class="h-6"
+			role="button"
+			aria-label="Switch to light mode"
+			tabindex="0"
+		/>
+		<Sun
+			v-else-if="colorMode.value === 'light'"
+			@click="colorMode.value = 'dark'"
+			class="h-6"
+			role="button"
+			aria-label="Switch to dark mode"
+			tabindex="0"
+		/>
 	</div>
 </template>
 
@@ -105,7 +103,7 @@ li a[aria-current="page"] {
 	position: fixed;
 	top: 1rem;
 	left: 50%;
-	transform: translateX(calc(-50% + 11.5rem));
+	transform: translateX(calc(-50% + 12rem));
 	background-color: var(--nav-background);
 
 	height: calc(3.5rem);
@@ -118,18 +116,5 @@ li a[aria-current="page"] {
 	z-index: 1000;
 
 	@apply rounded-xl backdrop-blur-sm shadow-lg hidden sm:flex;
-}
-
-.time-widget svg {
-	height: 50%;
-	width: 50%;
-	fill: var(--on-surface);
-}
-
-#hour-hand,
-#minute-hand,
-#second-hand {
-	transform-origin: inherit;
-	transition: transform 0.5s ease-in-out;
 }
 </style>
