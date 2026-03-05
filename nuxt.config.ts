b83d64bb98b91a41b015dbb950474083b0ad7dc3
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-03-05 08:08:45 +0100

    chore: update package.json to add linting and formatting scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,74 +1,75 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-04-03",
-	devtools: { enabled: true },
-	modules: [
-		"@nuxt/image",
-		"@nuxtjs/tailwindcss",
-		"@nuxtjs/seo",
-		"@nuxtjs/i18n",
-		"@nuxt/content",
-		"@nuxtjs/color-mode",
-	],
-	css: ["~/assets/css/reset.css", "~/assets/css/main.css", "~/assets/css/prose.css"],
-	i18n: {
-		locales: [
-			{ code: "en", language: "en-US", file: "en-US.json" },
-			{ code: "fr", language: "fr-FR", file: "fr-FR.json" },
-		],
-		baseUrl: "https://jeremie-zurfluh.ch",
-		defaultLocale: "en",
-		strategy: "prefix_and_default",
-		lazy: true,
-		langDir: "locales/",
-		bundle: {
-			optimizeTranslationDirective: false,
-		},
-	},
-	content: {
-		build: {
-			markdown: {
-				toc: {
-					searchDepth: 3,
-					depth: 3,
-				},
-			},
-		},
-	},
-	app: {
-		head: {
-			templateParams: {
-				separator: "|",
-				// other common separators: '·', '—', '•'
-			},
-		},
-	},
-	site: {
-		url: "https://jeremie-zurfluh.ch",
-		name: "Jérémie Zurflüh",
-	},
-	ogImage: {
-		enabled: false,
-	},
-	sitemap: {
-		enabled: false,
-		credits: false,
-	},
-	robots: {
-		enabled: true,
-	},
-	seo: {
-		enabled: true,
-	},
-	schemaOrg: {
-		enabled: true,
-		identity: "Person",
-	},
-	linkChecker: {
-		enabled: true,
-		failOnError: false,
-		report: {
-			markdown: true,
-		},
-	},
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/seo",
+    "@nuxtjs/i18n",
+    "@nuxt/content",
+    "@nuxtjs/color-mode",
+    "@nuxt/eslint",
+  ],
+  css: ["~/assets/css/reset.css", "~/assets/css/main.css", "~/assets/css/prose.css"],
+  i18n: {
+    locales: [
+      { code: "en", language: "en-US", file: "en-US.json" },
+      { code: "fr", language: "fr-FR", file: "fr-FR.json" },
+    ],
+    baseUrl: "https://jeremie-zurfluh.ch",
+    defaultLocale: "en",
+    strategy: "prefix_and_default",
+    lazy: true,
+    langDir: "locales/",
+    bundle: {
+      optimizeTranslationDirective: false,
+    },
+  },
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          searchDepth: 3,
+          depth: 3,
+        },
+      },
+    },
+  },
+  app: {
+    head: {
+      templateParams: {
+        separator: "|",
+        // other common separators: '·', '—', '•'
+      },
+    },
+  },
+  site: {
+    url: "https://jeremie-zurfluh.ch",
+    name: "Jérémie Zurflüh",
+  },
+  ogImage: {
+    enabled: false,
+  },
+  sitemap: {
+    enabled: false,
+    credits: false,
+  },
+  robots: {
+    enabled: true,
+  },
+  seo: {
+    enabled: true,
+  },
+  schemaOrg: {
+    enabled: true,
+    identity: "Person",
+  },
+  linkChecker: {
+    enabled: true,
+    failOnError: false,
+    report: {
+      markdown: true,
+    },
+  },
 });
